1a12c8a229a2131ccb28723b3ec6bf3390ea928e
Lara <152970053+LaraRF@users.noreply.github.com>
2024-06-04 19:47:38 +0200

    enemy moves along a set path (but super fast)

--- src/enemies.cpp
+++ src/enemies.cpp
@@ -3,30 +3,68 @@
 //
 
 #include "enemies.h"
-void enemies::update()  {
+
+/*
+//timer to have the enemies pause between steps instead of running nonstop
+//(re-)start a timer
+void enemies::StartTimer(Timer *timer, float pausetime) {
+    if (timer != nullptr) { //timer has to be more than 0
+        timer->Pausetime = pausetime;
+    }
+}
+
+void enemies::UpdateTimer(Timer *timer) {
+    if (timer != nullptr && timer->Pausetime > 0) {
+        timer->Pausetime -= GetFrameTime(); //subtracts current frame from the timer (if timer isn't already at 0)
+    }
+}
+
+bool enemies::TimerDone(Timer *timer) {
+    if (timer != nullptr) {
+        return timer->Pausetime <= 0;//returns true when the timer is done
+    }else {return false;}
+}*/
+
+void enemies::update() {
 
     /* Timer += Timer->GetTimeElapsed();
      int Random = (rand() % 4);
      if (Timer >= 2.0f)*/
 
-    //movement
-    if (IsKeyDown(KEY_G)) {
-        position.y = position.y + stepsize;
+    //movement with timer for the current enemy
+    switch (direction) {
+        case left:
+            do {
+                position.x = position.x - 1;
+            } while (position.x > (10 * 32 - 1));
+            direction = down;
+            break;
+        case down:
+            do {
+                position.y = position.y + 1;
+            } while (position.y < (12 * 32 + 1));
+            direction = right;
+            break;
+        case right:
+            do {
+                position.x = position.x + 1;
+            } while (position.x < (14 * 32 + 1));
+            direction = up;
+            break;
+        case up:
+            do {
+                position.y = position.y - 1;
+            } while (position.y > (10 * 32 - 1));
+            direction = left;
+            break;
     }
-    if (IsKeyDown(KEY_T)) {
-        position.y = position.y - stepsize;
-    }
-    if (IsKeyDown(KEY_F)) {
-        position.x = position.x - stepsize;
-    }
-    if (IsKeyDown(KEY_H)) {
-        position.x = position.x + stepsize;
-    }//end movement code
+    //end movement code
 
     for (int i = 0; _scene->touchesWall(position, size) && i < 4; i++) {
         Rectangle touchedWall = _scene->getTouchedWall(position, size);
         Vector2 touchPoint = Vector2Clamp(position, {touchedWall.x, touchedWall.y},
-                                          {touchedWall.x + touchedWall.width, touchedWall.y + touchedWall.height});
+                                          {touchedWall.x + touchedWall.width,
+                                           touchedWall.y + touchedWall.height});
         Vector2 pushForce = Vector2Subtract(position, touchPoint);
         float overlapDistance = size - Vector2Length(pushForce);
         if (overlapDistance <= 0) {
@@ -37,6 +75,7 @@ void enemies::update()  {
         position = Vector2Add(position, pushForce);
     }
 }
+
 void enemies::draw() {
 
     DrawTexture(enemyTexture1, position.x, position.y, WHITE);
@@ -44,6 +83,7 @@ void enemies::draw() {
 
 }
 
-enemies::enemies(gameplay *scene) : _scene(scene){
+enemies::enemies(gameplay *scene) : _scene(scene) {
 
 }
+
--- src/enemies.h
+++ src/enemies.h
@@ -13,21 +13,35 @@ class gameplay;
 
 class enemies {
 public:
-    float damage =0;
+    float damage = 0;
+
     void draw(); //method to draw enemy texture in base class
     void update();
+
     //movement
     float stepsize = 1.0f;
-    gameplay* _scene;
+    gameplay *_scene;
 
     //enemy map interaction > same as in maincharacter.h?
     enemies(gameplay *scene);
-    Vector2 position = {124, 32 * 7};
+
+    Vector2 position = {14 * 32, 32 * 10};
     float size = 16;
 
     Texture2D enemyTexture1 = assestmanagergraphics::getTexture("characters/enemies/enemy_1");
     //Texture2D enemyTexture2 = assestmanagergraphics::getTexture("characters/enemies/enemy_2");
+
+    enum direction {
+        left, right, up, down
+    };
+    direction direction = left;
+/*
+    struct Timer{float Pausetime=2.0f;};
+    static void StartTimer(Timer *timer,float pausetime);
+    static void UpdateTimer(Timer*timer);
+    static bool TimerDone(Timer*timer);
+    float enemypause =2.0f;
+    Timer enemytimer={0};*/
+
 };
-
-
 #endif //RAYLIBSTARTER_ENEMIES_H
