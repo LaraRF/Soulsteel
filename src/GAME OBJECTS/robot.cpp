adbbda21b936fa94c53ab233af05628aaa905c03
Lara <152970053+LaraRF@users.noreply.github.com>
2024-06-19 16:01:18 +0200

    update robot cpp, visible texture matches actual position of the robot

--- src/GAME OBJECTS/robot.cpp
+++ src/GAME OBJECTS/robot.cpp
@@ -3,48 +3,50 @@
 //
 
 #include "robot.h"
+#include "../maincharacter.h"
 
 void robot::update() {
 
-    switch(currentmode){
+    switch (currentmode) {
         case soulmodus:
-            if(IsKeyPressed(KEY_SPACE)){
-                currentmode=robotmodus;
+            if (IsKeyPressed(KEY_SPACE)) {
+                currentmode = robotmodus;
             }
             break;
         case robotmodus:
-            if(IsKeyPressed(KEY_SPACE)){
-                currentmode=soulmodus;
+            if (IsKeyPressed(KEY_SPACE)) {
+                currentmode = soulmodus;
             }
             break;
     }
 
-if(currentmode==robotmodus){
-    if (IsKeyDown(KEY_S)) {
-        position.y = position.y + stepsize;
-        lookingdirection=south;
+    if (currentmode == robotmodus) {
+        if (IsKeyDown(KEY_S)) {
+            position.y = position.y + stepsize;
+            lookingdirection = south;
+        }
+        if (IsKeyDown(KEY_W)) {
+            position.y = position.y - stepsize;
+            lookingdirection = north;
+        }
+        if (IsKeyDown(KEY_A)) {
+            position.x = position.x - stepsize;
+            lookingdirection = west;
+        }
+        if (IsKeyDown(KEY_D)) {
+            position.x = position.x + stepsize;
+            lookingdirection = east;
+        }//end movement code
     }
-    if (IsKeyDown(KEY_W)) {
-        position.y = position.y - stepsize;
-        lookingdirection=north;
-    }
-    if (IsKeyDown(KEY_A)) {
-        position.x = position.x - stepsize;
-        lookingdirection=west;
-    }
-    if (IsKeyDown(KEY_D)) {
-        position.x = position.x + stepsize;
-        lookingdirection=east;
-    }//end movement code
-}
+
 }
 
 void robot::draw() {
-    if(currentmode==robotmodus){
-        DrawTexture(characterRobotTexture, position.x-16, position.y-32, WHITE);
+    if (currentmode == robotmodus) {
+        DrawTexture(characterRobotTexture, position.x - 16, position.y - 32, WHITE);
         DrawCircle(position.x, position.y, size, GRAY);
     }
-    if(currentmode==soulmodus){
+    if (currentmode == soulmodus) {
         DrawCircle(position.x, position.y, size, BLACK);
     }
 
