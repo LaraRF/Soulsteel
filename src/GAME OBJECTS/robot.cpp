2ceaaf7460b40e2cd28cd12af8e770d17b690752
Lara <152970053+LaraRF@users.noreply.github.com>
2024-06-19 11:57:13 +0200

    added robot class

--- src/GAME OBJECTS/robot.cpp
+++ src/GAME OBJECTS/robot.cpp
@@ -3,17 +3,3 @@
 //
 
 #include "robot.h"
-#include "../gameplay.h"
-
-void robot::update() {
-
-}
-
-void robot::draw() {
-    DrawCircle(position.x, position.y, size,GRAY);
-    DrawTexture(characterRobotTexture, position.x-16, position.y-24, WHITE);
-}
-
-robot::robot() {
-
-}
--- src/GAME OBJECTS/robot.h
+++ src/GAME OBJECTS/robot.h
@@ -5,30 +5,8 @@
 #ifndef RAYLIBSTARTER_ROBOT_H
 #define RAYLIBSTARTER_ROBOT_H
 
-#include "raylib.h"
-#include "gameobjects.h"
-#include "../assestmanagergraphics.h"
-#include "../maincharactermodus.h"
-#include "../gameplay.h"
 
-class gameplay;
-
-class robot:public gameobjects {
-private:
-    Texture2D characterRobotTexture = assestmanagergraphics::getTexture("characters/robot/Character_-_Robot_-_Idle_Front_-_animated");
-
-public:
-    void update() override;
-    void draw() override;
-
-    Vector2 position = {12*32, 8*32};
-    float size = 12;
-
-    maincharactermodus currentmode=soulmodus;
-
-    robot();
-
-
+class robot {
 
 };
 
